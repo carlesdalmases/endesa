bef8dd239a54832d3196b9fbe85031f70f32584a
carlesdalmases <c.dalmases@gmail.com>
2020-03-04 19:32:12 +0100

    Tractament de l'hora i el dia

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Conversor de CSV d'ENDESA a InlfuxDB
-
+const parse = require('date-fns/parse');
+// const format = require('date-fns/format');
+// const formatISO = require('date-fns/formatISO');
+const {es} = require('date-fns/locale');
+const addDays = require('date-fns/addDays');
 const csv = require('csv');
 const fs = require('fs');
+
 
 const filepath = './dades.csv';
 const opt = {delimiter: ';',
@@ -13,7 +18,27 @@ const parser = csv.parse(opt);
 fs.createReadStream(filepath)
     .pipe(parser)
     .on('data', (row) => {
-      console.log(row);
+      let d;
+      d = parse(row[1]+' '+row[2], 'dd/MM/yyyy k', new Date(), {locale: es});
+
+      // Amb PARSE la única opció que admet l'hora 24 és k.
+      // El problema és que interpreta el dia 1 24:00 com el dia 1 00:00
+      // quan hauria d'interpretar dia 2 00:00
+      // En els CSV d'ENDESA, el consum és a hora passada, així la dada de
+      // consum de dia 1 24:00 correspon al consum de les 23:00 a 24:00.
+      // Quan la hora és 24:00, sumo un dia (i ell ja posa les 00:00h).
+
+      if (row[2] == 24) {
+        d = addDays(d, 1);
+      }
+
+      // console.log('Original: '+row[1]+' '+row[2]);
+      // console.log('AMB locale: '+
+      // format(d, 'dd/MM/yyyy, HH:mm:ss', {locale: es}));
+      // console.log('SENSE locale: '+
+      // format(d, 'dd/MM/yyyy, HH:mm:ss'));
+      // console.log('formatISO: '+formatISO(d));
+      // // console.log(row);
     })
 
     .on('end', () => {
